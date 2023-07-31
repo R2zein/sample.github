bd139e1c2204a5905753373c1315c6de2e520cb3
R2zein <133511703+R2zein@users.noreply.github.com>
2023-07-31 10:36:19 +0700

    Add files via upload

---
+++ latihan3/js/js-image-slider.js
@@ -0,0 +1,19 @@
+var sliderOptions=
+{
+	sliderId: "slider",
+	startSlide: 0,
+	effect: "series1",
+	effectRandom: false,
+	pauseTime: 1000,
+	transitionTime: 500,
+	slices: 12,
+	boxes: 8,
+	hoverPause: 1,
+	autoAdvance: true,
+	thumbnailsWrapperId: "thumbs",
+	m: false,
+	license: "mylicense"
+};
+
+var imageSlider=new mcImgSlider(sliderOptions);
+
